03d37bd30ca619203fed6ffc4dc618d65f507d41
ILARIO-KAGOO <ilariokagoo5047.sse@saveetha.com>
2024-03-18 08:48:57 +0530

    EXP-15

--- Programs/15.min_max_alg.py
+++ Programs/15.min_max_alg.py
@@ -7,85 +7,37 @@ MAX, MIN = 1000, -1000
  
 # Returns optimal value for current player 
 #(Initially called for root and maximizer) 
-
-def minimax(depth, nodeIndex, maximizingPlayer, 
-
-            values, alpha, beta): 
-
-  
-
+def minimax(depth, nodeIndex, maximizingPlayer, values, alpha, beta): 
     # Terminating condition. i.e 
-
     # leaf node is reached 
-
     if depth == 3: 
-
         return values[nodeIndex] 
- 
 
     if maximizingPlayer: 
-
-      
-
         best = MIN
- 
-
-        # Recur for left and right children 
-
+        # Recur for left and right children
         for i in range(0, 2): 
-
-             
-
-            val = minimax(depth + 1, nodeIndex * 2 + i, 
-
-                          False, values, alpha, beta) 
-
+            val = minimax(depth + 1, nodeIndex * 2 + i, False, values, alpha, beta) 
             best = max(best, val) 
-
             alpha = max(alpha, best) 
- 
 
             # Alpha Beta Pruning 
-
             if beta <= alpha: 
-
                 break
-
-          
-
         return best 
-
-      
-
     else:
-
         best = MAX
  
-
         # Recur for left and 
-
         # right children 
-
         for i in range(0, 2): 
-
-          
-
-            val = minimax(depth + 1, nodeIndex * 2 + i, 
-
-                            True, values, alpha, beta) 
-
+            val = minimax(depth + 1, nodeIndex * 2 + i, True, values, alpha, beta) 
             best = min(best, val) 
-
             beta = min(beta, best) 
  
-
             # Alpha Beta Pruning 
-
             if beta <= alpha: 
-
                 break
-
-          
 
         return best 
 
