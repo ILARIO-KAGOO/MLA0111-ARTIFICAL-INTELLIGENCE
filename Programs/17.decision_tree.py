fd605661307246553629299f81bd06d4cdd4741d
ILARIO-KAGOO <ilariokagoo5047.sse@saveetha.com>
2024-03-18 08:49:30 +0530

    EXP-17

--- Programs/17.decision_tree.py
+++ Programs/17.decision_tree.py
@@ -1,14 +1,16 @@
-import numpy
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
 
-iris= pd.read_csv("Datasets/iris.csv")
+from sklearn.datasets import load_iris
+db= load_iris()
+iris = pd.DataFrame(data= np.c_[db['data'], db['target']], columns= db['feature_names'] + ['Species'])
 print(iris.head())
 
-x = iris.drop("species", axis=1)
-y = iris["species"]
+x = iris.drop("Species", axis=1)
+y = iris["Species"]
 
 x_train, x_test, y_train, y_test = train_test_split(x, y,test_size=0.2,random_state=42)
 
@@ -16,5 +18,5 @@ x_train, x_test, y_train, y_test = train_test_split(x, y,test_size=0.2,random_st
 decisiontree = DecisionTreeClassifier()
 
 decisiontree.fit(x_train, y_train)
-x=decisiontree.predict(x_test)
-print(x)
+y=decisiontree.predict(x_test)
+print(accuracy_score(y,y_test))
